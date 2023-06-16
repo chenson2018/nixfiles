7c1eaef607292d0989a5d70d518df5ebc9462e11
Chris Henson <chenson@pop-os.localdomain>
2023-06-16 04:44:28 -0400

    lsp working

--- home.nix
+++ home.nix
@@ -29,6 +29,95 @@
 
     plugins = with pkgs.vimPlugins; [
       {
+        plugin = rose-pine;
+        config = "colorscheme rose-pine";
+      }
+      nvim-lspconfig
+      luasnip
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp_luasnip
+      {
+        plugin = nvim-cmp;
+        type = "lua";
+        config = ''
+                    local cmp = require('cmp')
+                    local cmp_select_opts = {behavior = cmp.SelectBehavior.Select}
+                    
+                    cmp.setup({
+                      sources = {
+                        {name = 'nvim_lsp'},
+                      },
+                      mapping = {
+                        ['<C-y>'] = cmp.mapping.confirm({select = true}),
+                        ['<C-e>'] = cmp.mapping.abort(),
+                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                        ['<Up>'] = cmp.mapping.select_prev_item(cmp_select_opts),
+                        ['<Down>'] = cmp.mapping.select_next_item(cmp_select_opts),
+                        ['<C-p>'] = cmp.mapping(function()
+                          if cmp.visible() then
+                            cmp.select_prev_item(cmp_select_opts)
+                          else
+                            cmp.complete()
+                          end
+                        end),
+                        ['<C-n>'] = cmp.mapping(function()
+                          if cmp.visible() then
+                            cmp.select_next_item(cmp_select_opts)
+                          else
+                            cmp.complete()
+                          end
+                        end),
+                      },
+                      snippet = {
+                        expand = function(args)
+                          require('luasnip').lsp_expand(args.body)
+                        end,
+                      },
+                      window = {
+                        documentation = {
+                          max_height = 15,
+                          max_width = 60,
+                        }
+                      },
+                      formatting = {
+                        fields = {'abbr', 'menu', 'kind'},
+                        format = function(entry, item)
+                          local short_name = {
+                            nvim_lsp = 'LSP',
+                            nvim_lua = 'nvim'
+                          }
+                    
+                          local menu_name = short_name[entry.source.name] or entry.source.name
+                    
+                          item.menu = string.format('[%s]', menu_name)
+                          return item
+                        end,
+                      },
+                    })
+          				'';
+      }
+      {
+        plugin = rust-tools-nvim;
+        type = "lua";
+        config = ''
+                    local rt = require("rust-tools")
+                    
+                    rt.setup({
+                      server = {
+                        on_attach = function(_, bufnr)
+                          -- Hover actions
+                          vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                          -- Code action groups
+                          vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                        end,
+                      },
+                    })
+          				'';
+      }
+      {
         plugin = telescope-nvim;
         type = "lua";
         config = ''
