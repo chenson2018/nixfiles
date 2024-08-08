1b9211ba79aefc74d3588671586484d0ed42a8a2
Chris Henson <chrishenson.net@gmail.com>
2024-08-08 12:45:21 -0400

    more website stuff

--- home.nix
+++ home.nix
@@ -344,6 +344,9 @@
     pkgs.rubber
     pkgs.poppler_utils 
 
+    # syntax highlighting for website
+    pkgs.python312Packages.pygments
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
